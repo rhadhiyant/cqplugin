e01f2da4c5cb381f12ab395c7ccd6638c4e8eca7
Harry Nirsine <harry@nirsine.com>
2021-01-18 16:32:46 +0700

    Applying real functionality

--- src/ios/cqplugin.m
+++ src/ios/cqplugin.m
@@ -9,6 +9,8 @@
 - (void)coolMethod:(CDVInvokedUrlCommand*)command;
 @end
 
+
+
 @implementation cqplugin
 
 - (void)coolMethod:(CDVInvokedUrlCommand*)command
@@ -25,38 +27,18 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
-- (void)say:(NSString*)word
+- (void)say:(CDVInvokedUrlCommand*)command
 {
+    NSString* word = [command.arguments objectAtIndex:0];
     NSLog(@"cq says = %@", word);
 }
 
-- (void)allowScormAccess:(NSString*)url
+- (void)allowScormAccess:(CDVInvokedUrlCommand*)command
 {
-    // simulator
-    /*
-    NSString* preUrl = [[[[[request.URL URLByDeletingLastPathComponent] URLByDeletingLastPathComponent] URLByDeletingLastPathComponent] absoluteString] stringByReplacingOccurrencesOfString:@"/Bundle/" withString:@"/Data/"];
-    preUrl = [NSString stringWithFormat:@"%@Documents/", preUrl];
-    */
-    
-    // iphone (not working)
-    /*
-    NSString* appId = [[[[request.URL URLByDeletingLastPathComponent] URLByDeletingLastPathComponent] URLByDeletingLastPathComponent] lastPathComponent];
-    NSString* preUrl = [@"file:///var/mobile/Containers/Data/Application/cq_app_id/Documents/" stringByReplacingOccurrencesOfString:@"cq_app_id" withString:appId];
-    
-    NSURL* scorm = [NSURL URLWithString:preUrl];
-    NSURL* scormAccess = [NSURL URLWithString:preUrl];
-     */
-    
-    // for all using url parameter
+    NSString* url = [command.arguments objectAtIndex:0];
     NSURL* scorm = [NSURL URLWithString:url];
     NSURL* scormAccess = [scorm URLByDeletingLastPathComponent];
-    
-    // debug
-    NSLog(@"cqdebug scorm = %@", scorm);
-    NSLog(@"cqdebug scormAccess = %@", scormAccess);
-    
-    // main part
-    // [(WKWebView*)_engineWebView loadFileURL:scorm allowingReadAccessToURL:scormAccess];
+    [(WKWebView*)_engineWebView loadFileURL:scorm allowingReadAccessToURL:scormAccess];
 }
 
 @end
